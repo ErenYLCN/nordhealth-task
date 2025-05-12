9fc2b8b6bed7e5eb277dc09c7141207b11d633b7
Eren Yalçın <yalcineren97@gmail.com>
2025-05-12 09:29:41 +0300

    setup `provetcloud`

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <provet-button variant="primary"> Count is: </provet-button>
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+import "@provetcloud/css";
+import "@provetcloud/web-components";
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  ssr: false,
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/test-utils']
-})
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/test-utils"],
+});
